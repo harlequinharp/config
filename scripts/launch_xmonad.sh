39cf2a5972a1977b0ebb4de20d4868bdf7bf2c18
abresee <bresee.al@husky.neu.edu>
2012-12-30 18:55:56 -0500

    configured laptop, vim

--- scripts/launch_xmonad.sh
+++ scripts/launch_xmonad.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-# startx 
-# Load resources
-xrdb -merge .Xresources
- 
-# Set up an icon tray
-trayer --edge top --align right --SetDockType true --SetPartialStrut true \
- --expand true --width 5 --transparent true --alpha 0 --tint 0x0000 --height 17 &
- 
-# Set the background 
-feh --bg-fill /home/allie/Dropbox/backgrounds/crow.jpg
 
-# Fire up apps
-dbus-launch thunar --daemon &
-pidgin &
-dropboxd &
-xscreensaver -no-splash &
-xsetroot -cursor_name left_ptr
- 
+startup_xmonad.sh
 # start xmonad
 exec dbus-launch xmonad
---
+++ scripts/startup_xmonad.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# startx 
+# Load resources
+xrdb -merge .Xresources
+ 
+# Set up an icon tray
+trayer --edge top --align right --SetDockType true --SetPartialStrut true \
+ --expand true --width 5 --transparent true --alpha 0 --tint 0x0000 --height 17 &
+ 
+# Set the background 
+feh --bg-fill /home/allie/Dropbox/backgrounds/crow.jpg
+
+# Fire up apps
+# dbus-launch thunar --daemon &
+pidgin &
+dropboxd &
+xsetroot -cursor_name left_ptr
+ 
