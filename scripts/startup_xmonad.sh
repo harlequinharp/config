ba5b5c9b9ea95d0d2117342f796b4de005e11006
abresee <bresee.al@husky.neu.edu>
2013-01-04 00:42:20 -0500

    :3

--- scripts/startup_xmonad.sh
+++ scripts/startup_xmonad.sh
@@ -4,7 +4,7 @@ xrdb -merge .Xresources
  
 # Set up an icon tray
 trayer --edge top --align right --SetDockType true --SetPartialStrut true \
- --expand true --width 5 --transparent true --alpha 0 --tint 0x0000 --height 17 &
+ --expand true --width 10 --transparent true --alpha 0 --tint 0x333333 --height 17 &
  
 # Set the background 
 feh --bg-fill /home/allie/Dropbox/backgrounds/crow.jpg
