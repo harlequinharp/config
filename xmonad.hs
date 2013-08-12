be76a97cea3ed49ae67f5c807d9d2f20253058e0
abresee <bresee.al@gmail.com>
2013-08-11 20:53:04 -0400

    okay acer's doing okay it seems

--- xmonad.hs
+++ xmonad.hs
@@ -6,44 +6,45 @@ import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 import System.IO
 
-myWorkspaces = ["1","2","3","4","5","6","7","8","9","0"] 
-
 modm = mod1Mask -- so we can change it in one location
 
-myManageHook = composeAll
-    [ className =? "Pidgin" --> doShift "2" -- All pidgin windows will spawn on workspace 2
-    , className =? "thunderbird" --> doShift "3" -- icedove windows spawn on ws 3
+myManageHook = composeAll [ 
+    className =? "Pidgin" --> doShift "2",
+    className =? "Thunderbird" --> doShift "3"
     ]
 
-numPadKeys = [ xK_KP_End,   xK_KP_Down,     xK_KP_Page_Down -- 1, 2, 3
-             , xK_KP_Left,  xK_KP_Begin,    xK_KP_Right     -- 4, 5, 6
-             , xK_KP_Home,  xK_KP_Up,       xK_KP_Page_Up   -- 7, 8, 9
-             , xK_KP_Insert ] -- 0
+myWorkspaces = ["1","2","3","4","5","6","7","8","9","0"] 
+
+numPadKeys = [ 
+    xK_KP_End,   xK_KP_Down,     xK_KP_Page_Down, -- 1, 2, 3
+    xK_KP_Left,  xK_KP_Begin,    xK_KP_Right,     -- 4, 5, 6
+    xK_KP_Home,  xK_KP_Up,       xK_KP_Page_Up,   -- 7, 8, 9
+    xK_KP_Insert                                  -- 0
+] 
+
+tp_toggle = "/home/allie/config/scripts/trackpad_toggle.sh"
 
 myKeys = [
-         ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s") -- ctrl+printscrn = screenshot area
-        , ((0, xK_Print), spawn "scrot") -- printscrn = screenshot
-        , ((modm .|. shiftMask, xK_r), spawn "/home/allie/config/scripts/trackpad_toggle.sh")
-        , ((modm .|. shiftMask, xK_w), spawn "firefox")
-        , ((modm .|. shiftMask, xK_f), spawn "thunar")
-        ]
-        ++
-        [((m .|. modm, k), windows $ f i) -- this block sets up numpad workspace switching but idgi
-            | (i, k) <- zip myWorkspaces numPadKeys
-            , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-        ]
+    ((modm .|. shiftMask, xK_r), spawn tp_toggle),
+    ((modm .|. shiftMask, xK_w), spawn "firefox"),
+    ((modm .|. shiftMask, xK_f), spawn "thunar"),
+    ((modm .|. shiftMask, xK_p), spawn "pidgin"),
+    ((m .|. modm, k), windows $ f i) 
+        | (i, k) <- zip myWorkspaces numPadKeys
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+]
 
 main = do 
     xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig
-        { manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-        , terminal = "xfce4-terminal"
-        , modMask = modm -- rebind mod to whatever modm is
-        , focusFollowsMouse = False
-        } `additionalKeys` myKeys
+    xmonad $ defaultConfig { 
+        manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig,
+        layoutHook = avoidStruts $ layoutHook defaultConfig,
+        logHook = dynamicLogWithPP xmobarPP { 
+            ppOutput = hPutStrLn xmproc,
+            ppTitle = xmobarColor "green" "" . shorten 50
+        },
+        terminal = "xfce4-terminal" ,
+        modMask = modm, -- rebind mod to whatever modm is 
+        focusFollowsMouse = False
+    } `additionalKeys` myKeys
     
