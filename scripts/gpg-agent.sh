d5ca8368881a33a2619469f74ec25f8b9c038bcc
abresee <bresee.al@gmail.com>
2013-08-31 04:33:45 -0400

    track gpg

---
+++ scripts/gpg-agent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+gnupginf="${HOME}/.gpg-agent-info"
+
+if pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
+    eval `cat $gnupginf`
+    eval `cut -d= -f1 $gnupginf | xargs echo export`
+else
+    eval `gpg-agent -s --enable-ssh-support --daemon --write-env-file "$gnupginf"`
+fi
