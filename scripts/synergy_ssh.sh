2c880ad0e96295cb2b83f069498f608b1b9f34cb
abresee <bresee.al@gmail.com>
2013-09-07 09:36:27 -0400

    ssh

--- scripts/synergy_ssh.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ssh -f -N -L localhost:24800:allie-desk:24800 allie-desk
-SYN_CMD='/usr/bin/synergyc -d FATAL --no-restart localhost'
-/usr/bin/pgrep -lxf "$SYN_CMD" || ( ( $SYN_CMD ) & )
