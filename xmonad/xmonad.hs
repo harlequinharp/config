a0e2e46fc0c407b34982e6276100460885aa6a9a
abresee <bresee.al@gmail.com>
2013-10-07 18:29:39 -0400

    change toggle struts to mod+shift+b from mod+b to avoid conflict with pcmanfm

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -112,7 +112,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $ [
     ((modMask,                  xK_f        ), spawn "pcmanfm"),
     ((modMask,                  xK_space    ), sendMessage NextLayout),
     ((modMask .|. shiftMask,    xK_space    ), setLayout $ XMonad.layoutHook conf),
-    ((modMask,                  xK_b        ), sendMessage ToggleStruts),
+    ((modMask .|. shiftMask,    xK_b        ), sendMessage ToggleStruts),
     ((modMask,                  xK_n        ), refresh),
     ((modMask,                  xK_Tab      ), windows W.focusDown),
     ((modMask,                  xK_j        ), windows W.focusDown),
